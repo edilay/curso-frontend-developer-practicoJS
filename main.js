7ef8d4d8e347ce11e20898c98e9d47d3a37f3480
Edilay Peña Osorio <edilay.pena@mvm.com.co>
2023-05-25 21:00:53 -0500

    Clase 2: Menú hamburguesa, Media Query para desktop-menu y mobile-menu

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+
+/*Aparece y desaparece el menú al hacer clic en el email.---------------------*/
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click',toggleDesktopMenu)
+
+/*Aparece y desaparece el menú al hacer clic en el botón hamburgesa.---------------------*/
+const btnMobileMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
+/*Aparece y desaparece el menú al hacer clic en el email.---------------------*/
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+/*Aparece y desaparece el menú al hacer clic en el botón hamburgesa.---------------------*/
+btnMobileMenu.addEventListener('click',toggleMobileMenu);
+
+/*Aparece y desaparece el menú al hacer clic en el email.---------------------*/
 function toggleDesktopMenu(){
-    console.log('Click');
+    console.log('Click Desktop Menú');
     desktopMenu.classList.toggle('inactive');
 }
 
+/*Aparece y desaparece el menú al hacer clic en el botón hamburgesa.---------------------*/
+function toggleMobileMenu(){
+    console.log('Click Mobile Menú');
+    mobileMenu.classList.toggle('inactive');
+}
